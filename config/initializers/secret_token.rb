0622c2b6f57e4ebd28b8cbeb701f04167e559238
Jeremy Meyer <jjman505@gmail.com>
2014-03-16 15:11:16 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LiveData::Application.config.secret_key_base = '8a6ac6939e63cd5daa86f457b35e929aadcf3da27151043167a36311413e28ba06530dd170fbc4a2aceea28bcaa2642744f249fdf555f75fc4a300ecb7114b67'
