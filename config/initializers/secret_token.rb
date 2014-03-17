04e64e27a4437490127a6771214d59b6b4c66c53
Jeremy Meyer <jjman505@gmail.com>
2014-03-17 11:19:09 -0400

    changed secret key gen and readme

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Effortlist::Application.config.secret_key_base = '8a6ac6939e63cd5daa86f457b35e929aadcf3da27151043167a36311413e28ba06530dd170fbc4a2aceea28bcaa2642744f249fdf555f75fc4a300ecb7114b67'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Effortlist::Application.config.secret_key_base = secure_token
